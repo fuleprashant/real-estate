c31cef9d8e1660d44dcc1bc1a15f91987f2579f5
Prashant Fule <fuleprashant2809@gmail.com>
2024-09-01 11:37:18 +0530

    middleware to handle the error

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,18 +1,15 @@
 import User from "../model/user.model.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utill/Error.js";
 
-export const SignUp = async (req, res ) => {
-   const { username, email, password } = req.body;
-   const hashedPassword = bcryptjs.hashSync(password,10)
-   const newUser = new User({ username, email, password: hashedPassword });
-try {
-   await newUser.save(); 
-   res.status(201).json("User created succesfully");
-   
-} catch (error) {
-   res.status(500).json(error.message)
-
-}
+export const SignUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created succesfully");
+  } catch (error) {
+    next(errorHandler(550,"This is my created Error"));
+  }
 };
-
-// this is the initial level of the base strath
--- backend/index.js
+++ backend/index.js
@@ -28,4 +28,13 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth",signUpRouter)
 
-
+// middleware for the errorhandling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
